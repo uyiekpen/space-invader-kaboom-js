80a549f3739e6cbb03f4e4798f4cec3d83fcd37f
Uyiekpen Osazie Elizabeth <71497866+uyiekpen@users.noreply.github.com>
2022-09-25 12:55:17 +0000

    game

---
+++ code/lose.js
@@ -0,0 +1,7 @@
+add([
+  text(args.score),
+  origin("center"),
+  pos(width() / 2, height() / 2),
+  area(),
+
+])
--- code/main.js
+++ code/main.js
@@ -10,12 +10,53 @@ loadSprite("space-invader", "sprites/space-invader.png")
 
 
 //add a character to screen
-add([
+const player = add([
   // list of components
   sprite("space-ship"),
-  pos(width()/2, height()/2),
+  pos(width() / 2, height() / 2),
   area(),
+  origin("center")
 ])
+
+const Move_Speed = 200
+
+keyDown("left", () => {
+  player.move(-Move_Speed, 0)
+})
+
+keyDown("right", () => {
+  player.move(Move_Speed, 0)
+})
+
+layer(["obj", "ui",], "obj")
+
+const score = add([
+  text("0"),
+  pos(50, 50),
+  layer("ui"),
+  {
+    value: 0
+  }
+])
+
+const TIME_LEFT = 14
+
+const timer = add([
+  text("0"),
+  pos(90, 50),
+  layer("ui"),
+  {
+    time: TIME_LEFT
+  }
+])
+
+timer.action(() => {
+  timer.time -= dt()
+  timer.text = timer.time.toFixed(2)
+  if (timer.time <= 0) {
+    go("lose", score.value)
+  }
+})
 
 addLevel([
   "!@@@@@@@@@@@@@@@  &",
